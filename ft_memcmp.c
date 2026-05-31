c3bdea81816e3f095edc58e60abfba271b8f36ae
caguiari <caterinaaguiari@gmail.com>
2026-05-31 13:41:36 +0200

    Update ft_memcmp.c

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -14,19 +14,18 @@
 
 int	ft_memcmp(const void *s1, const void *s2, size_t n)
 {
-	unsigned char	*p1;
-	unsigned char	*p2;
+	unsigned char	*str1;
+	unsigned char	*str2;
 	size_t			i;
 
-	if (s1 == NULL && s2 == NULL)
-		return (0);
-	p1 = (unsigned char *)s1;
-	p2 = (unsigned char *)s2;
+	i = 0;
+	str1 = (unsigned char *)s1;
+	str2 = (unsigned char *)s2;
 	i = 0;
 	while (i < n)
 	{
-		if (p1[i] != p2[i])
-			return (p1[i] - p2[i]);
+		if (str1[i] != str2[i])
+			return (str1[i] - str2[i]);//qui promuovi direttamente da unsigned char ad int. Ecco perche' viene fuori anche un negativo
 		i++;
 	}
 	return (0);
