9a124c81efa0fcc85fd596d761baf532b8fa33fd
caguiari <caterinaaguiari@gmail.com>
2026-05-31 13:35:56 +0200

    Update ft_strncmp.c

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -17,12 +17,12 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	size_t	i;
 
 	i = 0;
-	while (i < n && (s1[i] != '\0' || s2[i] != '\0'))
+	while (i < n)
 	{
-		if (s1[i] < s2[i])
-			return (-1);
-		if (s1[i] > s2[i])
-			return (1);
+		if (s1[i] != s2[i])//Se i caratteri sono diversi (incluso il caso in cui una stringa è finita e l'altra no), restituisce immediatamente la differenza corretta con il casting a unsigned char
+			return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+		if (s1[i] == '\0')//Se siamo arrivati qui, significa che s1[i] == s2[i]. Se s1[i] è \0, significa che anche s2[i] è \0. Le stringhe sono identiche e sono finite, quindi restituisce 0
+			return (0);
 		i++;
 	}
 	return (0);
