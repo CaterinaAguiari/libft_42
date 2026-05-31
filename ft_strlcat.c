4154903e7efbee77354a00bd8c9340906ad996fe
caguiari <caterinaaguiari@gmail.com>
2026-05-31 13:00:51 +0200

    Update ft_strlcat.c
    Added a function and changed the ft_strlcat one

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -12,24 +12,34 @@
 
 #include "libft.h"
 
+static size_t	ft_strlen_dst(const char *s, size_t maxlen)
+{
+	size_t	i;
+
+	i = 0;
+	while (i < maxlen && s[i] != '\0')
+		i++;
+	return (i);
+}
 size_t	ft_strlcat(char *dst, const char *src, size_t size)
 {
 	size_t	src_len;
 	size_t	dst_len;
 	size_t	i;
+	size_t	j;
 
+	j = 0;
 	src_len = ft_strlen(src);
-	dst_len = 0;
-	while (dst[dst_len] != 0 && dst_len < size) //calcola la lunghezza di dst senza superare size
-		dst_len++;
-	i = 0;
-	if (size <= dst_len) //se "dst" e' gia' lungo almeno "size"
-		return (size + src_len);
-	while (src[i] != '\0' && (dst_len + i + 1) < size) /*ora scorri src fino al \0 e finche' ci sono caratteri da copiare. Lascia sempre spazio per il \0. Mai superare size*/
+	dst_len = ft_strlen_dst(dst, size);
+	if (dst_len == size) //regola del man per cui strlcat returna src_len + dst_len, ma se la len di dst e' = size, returna la lunghezza di quello che dovrebbe venire fuori, meno il \0
+		return (dst_len + src_len);
+	i = dst_len;
+	while (src[i] != '\0' && i < (size - 1)) /*ora scorri src fino al \0 e finche' ci sono caratteri da copiare. Lascia spazio per \0. Ad ogni ciclo incrementa "i"
 	{
-		dst[dst_len + i] = src[i]; //qui copi src dentro dst
+		dst[i] = src[j]; //qui copi src dentro dst
 		i++;
+		j++;
 	}
-	dst[dst_len + i] = '\0'; //qui metti il \0 alla fine, dopo aver copiato tutto
+	dst[i] = '\0'; //qui metti il \0 alla fine, dopo aver copiato tutto
 	return (dst_len + src_len);
 }
