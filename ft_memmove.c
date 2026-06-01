6a56a6a0195ecaa12b93fb8237ff9452c50db225
caguiari <caterinaaguiari@gmail.com>
2026-06-01 12:50:04 +0200

    Update ft_memmove.c

--- ft_memmove.c
+++ ft_memmove.c
@@ -12,33 +12,39 @@
 
 #include "libft.h"
 
+static void	ft_copy_forward(unsigned char *d, const unsigned char *s, size_t n)
+{
+	size_t	i;
+
+	i = 0;
+	while (i < n)
+	{
+		d[i] = s[i];
+		i++;
+	}
+}
+
 void	*ft_memmove(void *dest, const void *src, size_t n)
 {
 	const unsigned char	*s;
-	unsigned char	*d;
-	size_t	i;
+	unsigned char		*d;
+	size_t				i;
 
 	if (dest == NULL && src == NULL)
 		return (dest);
 	d = (unsigned char *)dest;
 	s = (const unsigned char *)src;
-	if (d > s) //se dest e' piu' avanti in memoria rispetto a src
+	if (d > s)
 	{
-		i = n;//cosi' non modifichi il vero "n"
+		i = n;
 		while (i > 0)
 		{
-			i--;//cosi' eviti il \0
+			i--;
 			d[i] = s[i];
 		}
 	}
 	else
-	{
-		i = 0;
-		while (i > n)
-		{
-			d[i] = s[i];
-			i++;
-		}
-	}
+		ft_copy_forward(d, s, n);
 	return (dest);
 }
+
