909480de9b3184b97e0a79f2f7d6ab8c69dbfeff
caguiari <caterinaaguiari@gmail.com>
2026-06-01 15:37:04 +0200

    Update ft_strtrim.c

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -17,14 +17,16 @@ char	*ft_strtrim(char const *s1, char const *set)
 	size_t	end;
 	size_t	start;
 	size_t	trimmed_len;
+	int	set_nbr;
 
 	if (!s1 || !set)
 		return (NULL);
+	set_nbr = ft_atoi(set);
 	end = ft_strlen(s1);
 	start = 0;
-	while (s1[start] != '\0' && ft_strchr(s1[start], set))
+	while (s1[start] != '\0' && ft_strchr(&s1[start], set_nbr))
 		start++;
-	while (end > start && ft_strchr(s1[end - 1], set))
+	while (end > start && ft_strchr(&s1[end - 1], set_nbr))
 		end--;
 	trimmed_len = (end - start);
 	return (ft_substr(s1, start, trimmed_len));
